415896592564fb11bae99cae3f3a2d0118d9c2b7
sash04ek <a.ovcharov@gmail.com>
2017-03-23 17:00:36 +0300

    Added

--- Module.php
+++ Module.php
@@ -366,7 +366,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 					'password' => $sPassword
 				);
 				$this->broadcastEvent(
-					'CreateAccount', 
+					'CreateAccount::before', 
 					$aArgs,
 					$oEventResult
 				);
